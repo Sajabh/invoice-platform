c3a91b741d18a30ecf598fb2d3c1b4ad78735f96
Saja Hammad <saja3bh@gmail.com>
2025-04-10 12:35:07 +0200

    feat: Implement invoice PDF generation and editing functionality
    - Added a new API route for generating PDF invoices using jsPDF.
    - Created an EditInvoice component for editing invoice details with form validation.
    - Integrated invoice data fetching in the dashboard for editing invoices.
    - Enhanced user experience with currency formatting and dynamic total calculation.

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,11 +1,34 @@
 import CreateInvoice from "@/app/components/createInvoice";
+import requireUser from "@/app/utils/hooks";
+import { prisma } from "@/lib/prisma";
 
-const InvoicesCreationRouts = () => {
-    return ( 
-        <div>
-            <CreateInvoice />
-        </div>
-     );
+async function getUserData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      firstName: true,
+      lastName: true,
+      address: true,
+      email: true,
+    },
+  });
+
+  return data;
 }
- 
+
+const InvoicesCreationRouts = async () => {
+  const session = await requireUser();
+  const data = await getUserData(session.user?.id as string);
+  return (
+    <CreateInvoice
+      lastName={data?.lastName as string}
+      address={data?.address as string}
+      email={data?.email as string}
+      firstName={data?.firstName as string}
+    />
+  );
+};
+
 export default InvoicesCreationRouts;
