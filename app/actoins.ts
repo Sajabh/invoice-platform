55f2d2e3b8ecab2b0b1842063f1023a8ec8dca5e
Saja Hammad <saja3bh@gmail.com>
2025-04-19 19:30:58 +0300

    refactor(actions): clean up production functions and improve deployment organization

--- app/actoins.ts
+++ app/actoins.ts
@@ -94,7 +94,10 @@ export async function createInvoice(prevState: any, formData: FormData) {
         amount: submission.value.total,
         currency: submission.value.currency as any,
       }),
-      invoiceLink: `http://localhost:3000/api/invoice/${data.id}`,
+      invoiceLink:
+        process.env.NODE_ENV !== "production"
+          ? `http://localhost:3000/api/invoice/${data.id}`
+          : `https://invoice-platform-two.vercel.app//api/invoice/${data.id}`,
     },
   });
 
@@ -161,7 +164,10 @@ export async function editInvoice(prevState: any, formData: FormData) {
         amount: submission.value.total,
         currency: submission.value.currency as any,
       }),
-      invoiceLink: `http://localhost:3000/api/invoice/${data.id}`,
+      invoiceLink:
+        process.env.NODE_ENV !== "production"
+          ? `http://localhost:3000/api/invoice/${data.id}`
+          : `https://invoice-platform-two.vercel.app//api/invoice/${data.id}`,
     },
   });
 
